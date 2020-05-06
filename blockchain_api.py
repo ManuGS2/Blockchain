8357d25d02ef7ca2ef77773725e488f84a157766
Emmanuel Guzmán <42596826+ManuGS2@users.noreply.github.com>
2020-05-05 22:52:01 -0500

    Adding consensus

--- blockchain_api.py
+++ blockchain_api.py
@@ -38,7 +38,14 @@ def mine_txns():
     if not result:
         return "Nothing to mine", 200
     
-    return "Block #{} is being mined".format(result), 200
+    else:
+        chain_length = len(blockchain.chain)
+        consensus()
+
+        if chain_length == len(blockchain.chain):
+            announce_new_block(blockchain.last_block)
+
+        return "Block #{} is mined.".format(blockchain.last_block.index)
 
 
 @app.route("/pending_txn", methods=["GET"])
@@ -87,6 +94,31 @@ def register_with_existing_node():
         return response.content, response.status_code
 
 
+@app.route('/add_block', methods=['POST'])
+def verify_and_add_block():
+    block_data = request.get_json()
+    block = Block(
+        block_data["index"],
+        block_data["transactions"],
+        block_data["timestamp"],
+        block_data["previous_hash"]
+    )
+
+    proof = block_data['hash']
+    added = blockchain.add_block(block, proof)
+
+    if not added:
+        return "The block was discarded by the node", 400
+
+    return "Block added to the chain", 201
+
+
+def announce_new_block(block):
+    for peer in peers:
+        url = "{}/add_block".format(peer)
+        requests.post(url, data=json.dumps(block.__dict__, sort_keys=True))
+
+
 def create_chain_from_dump(chain_dump):
     blockchain = Blockchain()
     for idx, block_data in enumerate(chain_dump):
@@ -106,6 +138,28 @@ def create_chain_from_dump(chain_dump):
             blockchain.chain.append(block)
             
         return blockchain
+
+
+def consensus():
+    global blockchain
+    
+    longest_chain = None
+    current_len = len(blockchain)
+
+    for node in peers:
+        response = requests.get("{}/chain".format(node))
+        length = response.json().get("length")
+        chain = response.json().get("chain")
+
+        if length > current_len and blockchain.validate_chain(chain):
+            current_len = length
+            longest_chain = chain
+
+    if longest_chain:
+        blockchain = longest_chain
+        return True
+
+    return False
 
 
 
